ef4035c8a97303d2904ff09e27477d3cc16d2f09
conorbyrn <conor.byrn@gmail.com>
2018-04-23 12:51:31 +0100

    new commit

--- target/scala-2.12/twirl/main/views/html/Laptop.template.scala
+++ target/scala-2.12/twirl/main/views/html/Laptop.template.scala
@@ -43,13 +43,12 @@ Seq[Any](format.raw/*1.59*/("""
 	<link rel="stylesheet" type="text/css" href=""""),_display_(/*11.48*/routes/*11.54*/.Assets.versioned("stylesheets/myStyles.css")),format.raw/*11.99*/("""">
 	
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
 <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
@@ -134,72 +133,72 @@ Seq[Any](format.raw/*1.59*/("""
 				<div class="panel panel-primary">
 				  <div class="panel-heading">Dell</div>
 				  <div class="panel-body"><img src="/assets/images/dell-xps.jpg" alt="Alienware" class="productImg" style="height: 13em"></div>
-				  """),_display_(/*105.8*/for(p <-products) yield /*105.25*/ {_display_(Seq[Any](format.raw/*105.27*/("""
-					"""),_display_(/*106.7*/if(p.getId == 1)/*106.23*/ {_display_(Seq[Any](format.raw/*106.25*/("""
-				  """),format.raw/*107.7*/("""<h4>"""),_display_(/*107.12*/p/*107.13*/.getName),format.raw/*107.21*/("""</h4>
+				  """),_display_(/*104.8*/for(p <-products) yield /*104.25*/ {_display_(Seq[Any](format.raw/*104.27*/("""
+					"""),_display_(/*105.7*/if(p.getId == 1)/*105.23*/ {_display_(Seq[Any](format.raw/*105.25*/("""
+				  """),format.raw/*106.7*/("""<h4>"""),_display_(/*106.12*/p/*106.13*/.getName),format.raw/*106.21*/("""</h4>
 				  <ul>	
-					<li>"""),_display_(/*109.11*/p/*109.12*/.getDescription),format.raw/*109.27*/("""</li><br>
-					<li>"""),_display_(/*110.11*/p/*110.12*/.getStock),format.raw/*110.21*/("""</li><br>
-					<li>"""),_display_(/*111.11*/p/*111.12*/.getPrice),format.raw/*111.21*/("""</li><br>
+					<li>"""),_display_(/*108.11*/p/*108.12*/.getDescription),format.raw/*108.27*/("""</li><br>
+					<li>"""),_display_(/*109.11*/p/*109.12*/.getStock),format.raw/*109.21*/("""</li><br>
+					<li>"""),_display_(/*110.11*/p/*110.12*/.getPrice),format.raw/*110.21*/("""</li><br>
 					<td>
-						<a href=""""),_display_(/*113.17*/routes/*113.23*/.HomeController.deleteProduct(p.getId)),format.raw/*113.61*/("""" class="button-xs btn-danger">
-							<span class="glyphicon-trash"></span>
+						<a href=""""),_display_(/*112.17*/routes/*112.23*/.HomeController.deleteProduct(p.getId)),format.raw/*112.61*/("""" class="button-xs btn-danger">
+							<span class="glyphicon glyphicon-trash"></span>
 						</a>
 					</td>
 					
-					  """)))}),format.raw/*118.9*/("""
-				  """),format.raw/*119.7*/("""</ul>
-					""")))}),format.raw/*120.7*/("""
-				"""),format.raw/*121.5*/("""</div>
+					  """)))}),format.raw/*117.9*/("""
+				  """),format.raw/*118.7*/("""</ul>
+					""")))}),format.raw/*119.7*/("""
+				"""),format.raw/*120.5*/("""</div>
 			  </div>
 			  <div class="col-sm-4"> 
 				<div class="panel panel-danger">
 				  <div class="panel-heading">Alienware</div>
 				  <div class="panel-body"><img src="/assets/images/Alienware.png" alt="Alienware" class="productImg" style="height: 13em"></div>
-				  """),_display_(/*127.8*/for(p <-products) yield /*127.25*/ {_display_(Seq[Any](format.raw/*127.27*/("""
-					"""),_display_(/*128.7*/if(p.getId == 7)/*128.23*/ {_display_(Seq[Any](format.raw/*128.25*/("""
-				  """),format.raw/*129.7*/("""<h4>"""),_display_(/*129.12*/p/*129.13*/.getName),format.raw/*129.21*/("""</h4>
+				  """),_display_(/*126.8*/for(p <-products) yield /*126.25*/ {_display_(Seq[Any](format.raw/*126.27*/("""
+					"""),_display_(/*127.7*/if(p.getId == 7)/*127.23*/ {_display_(Seq[Any](format.raw/*127.25*/("""
+				  """),format.raw/*128.7*/("""<h4>"""),_display_(/*128.12*/p/*128.13*/.getName),format.raw/*128.21*/("""</h4>
 				  <ul>	
-					<li>"""),_display_(/*131.11*/p/*131.12*/.getDescription),format.raw/*131.27*/("""</li><br>
-					<li>"""),_display_(/*132.11*/p/*132.12*/.getStock),format.raw/*132.21*/("""</li><br>
-					<li>"""),_display_(/*133.11*/p/*133.12*/.getPrice),format.raw/*133.21*/("""</li><br>
+					<li>"""),_display_(/*130.11*/p/*130.12*/.getDescription),format.raw/*130.27*/("""</li><br>
+					<li>"""),_display_(/*131.11*/p/*131.12*/.getStock),format.raw/*131.21*/("""</li><br>
+					<li>"""),_display_(/*132.11*/p/*132.12*/.getPrice),format.raw/*132.21*/("""</li><br>
 					<td>
-						<a href=""""),_display_(/*135.17*/routes/*135.23*/.HomeController.deleteProduct(p.getId)),format.raw/*135.61*/("""" class="button-xs btn-danger">
-							<span class="glyphicon-trash"></span>
+						<a href=""""),_display_(/*134.17*/routes/*134.23*/.HomeController.deleteProduct(p.getId)),format.raw/*134.61*/("""" class="button-xs btn-danger">
+							<span class="glyphicon glyphicon-trash"></span>
 						</a>
 					</td>
 					
-					  """)))}),format.raw/*140.9*/("""
-				  """),format.raw/*141.7*/("""</ul>
-					""")))}),format.raw/*142.7*/("""
-				"""),format.raw/*143.5*/("""</div>
+					  """)))}),format.raw/*139.9*/("""
+				  """),format.raw/*140.7*/("""</ul>
+					""")))}),format.raw/*141.7*/("""
+				"""),format.raw/*142.5*/("""</div>
 			  </div>
 			  <div class="col-sm-4"> 
 				<div class="panel panel-success">
 				  <div class="panel-heading">Inspirion</div>
 				  <div class="panel-body"><img src="/assets/images/Inspiron.jpg" alt="Inspiron" class="productImg" style="height: 13em;"></div>
-				  """),_display_(/*149.8*/for(p <-products) yield /*149.25*/ {_display_(Seq[Any](format.raw/*149.27*/("""
-					"""),_display_(/*150.7*/if(p.getId == 4)/*150.23*/ {_display_(Seq[Any](format.raw/*150.25*/("""
-				  """),format.raw/*151.7*/("""<h4>"""),_display_(/*151.12*/p/*151.13*/.getName),format.raw/*151.21*/("""</h4>
+				  """),_display_(/*148.8*/for(p <-products) yield /*148.25*/ {_display_(Seq[Any](format.raw/*148.27*/("""
+					"""),_display_(/*149.7*/if(p.getId == 4)/*149.23*/ {_display_(Seq[Any](format.raw/*149.25*/("""
+				  """),format.raw/*150.7*/("""<h4>"""),_display_(/*150.12*/p/*150.13*/.getName),format.raw/*150.21*/("""</h4>
 				  <ul>	
-					<li>"""),_display_(/*153.11*/p/*153.12*/.getDescription),format.raw/*153.27*/("""</li><br>
-					<li>"""),_display_(/*154.11*/p/*154.12*/.getStock),format.raw/*154.21*/("""</li><br>
-					<li>"""),_display_(/*155.11*/p/*155.12*/.getPrice),format.raw/*155.21*/("""</li><br>
+					<li>"""),_display_(/*152.11*/p/*152.12*/.getDescription),format.raw/*152.27*/("""</li><br>
+					<li>"""),_display_(/*153.11*/p/*153.12*/.getStock),format.raw/*153.21*/("""</li><br>
+					<li>"""),_display_(/*154.11*/p/*154.12*/.getPrice),format.raw/*154.21*/("""</li><br>
 					<td>
-						<a href=""""),_display_(/*157.17*/routes/*157.23*/.HomeController.deleteProduct(p.getId)),format.raw/*157.61*/("""" class="button-xs btn-danger">
-							<span class="glyphicon-trash"></span>
+						<a href=""""),_display_(/*156.17*/routes/*156.23*/.HomeController.deleteProduct(p.getId)),format.raw/*156.61*/("""" class="button-xs btn-danger">
+							<span class="glyphicon glyphicon-trash"></span>
 						</a>
 					</td>
 					
-					  """)))}),format.raw/*162.9*/("""
-				  """),format.raw/*163.7*/("""</ul>
-					""")))}),format.raw/*164.7*/("""
-				"""),format.raw/*165.5*/("""</div>
+					  """)))}),format.raw/*161.9*/("""
+				  """),format.raw/*162.7*/("""</ul>
+					""")))}),format.raw/*163.7*/("""
+				"""),format.raw/*164.5*/("""</div>
 			  </div>
 			</div>
 			</div>
 			<p>
-				<a href=""""),_display_(/*170.15*/routes/*170.21*/.HomeController.addProduct()),format.raw/*170.49*/("""">
+				<a href=""""),_display_(/*169.15*/routes/*169.21*/.HomeController.addProduct()),format.raw/*169.49*/("""">
 					<button class="btn btn-primary">Add a product</button>
 				</a>
 			</p>
@@ -226,11 +225,11 @@ Seq[Any](format.raw/*1.59*/("""
 
               /*
                   -- GENERATED --
-                  DATE: Mon Apr 23 12:06:08 IST 2018
+                  DATE: Mon Apr 23 12:48:19 IST 2018
                   SOURCE: /home/wdd/webapps/FinishedProject/app/views/Laptop.scala.html
-                  HASH: 53121eb3c5a1704f142ba0417a7319227a7ae7a1
-                  MATRIX: 981->1|1133->58|1163->62|1306->178|1321->184|1387->229|4980->3795|5014->3812|5055->3814|5089->3821|5115->3837|5156->3839|5191->3846|5224->3851|5235->3852|5265->3860|5321->3888|5332->3889|5369->3904|5417->3924|5428->3925|5459->3934|5507->3954|5518->3955|5549->3964|5613->4000|5629->4006|5689->4044|5833->4157|5868->4164|5911->4176|5944->4181|6245->4455|6279->4472|6320->4474|6354->4481|6380->4497|6421->4499|6456->4506|6489->4511|6500->4512|6530->4520|6586->4548|6597->4549|6634->4564|6682->4584|6693->4585|6724->4594|6772->4614|6783->4615|6814->4624|6878->4660|6894->4666|6954->4704|7098->4817|7133->4824|7176->4836|7209->4841|7510->5115|7544->5132|7585->5134|7619->5141|7645->5157|7686->5159|7721->5166|7754->5171|7765->5172|7795->5180|7851->5208|7862->5209|7899->5224|7947->5244|7958->5245|7989->5254|8037->5274|8048->5275|8079->5284|8143->5320|8159->5326|8219->5364|8363->5477|8398->5484|8441->5496|8474->5501|8562->5561|8578->5567|8628->5595
-                  LINES: 28->1|33->1|37->5|43->11|43->11|43->11|137->105|137->105|137->105|138->106|138->106|138->106|139->107|139->107|139->107|139->107|141->109|141->109|141->109|142->110|142->110|142->110|143->111|143->111|143->111|145->113|145->113|145->113|150->118|151->119|152->120|153->121|159->127|159->127|159->127|160->128|160->128|160->128|161->129|161->129|161->129|161->129|163->131|163->131|163->131|164->132|164->132|164->132|165->133|165->133|165->133|167->135|167->135|167->135|172->140|173->141|174->142|175->143|181->149|181->149|181->149|182->150|182->150|182->150|183->151|183->151|183->151|183->151|185->153|185->153|185->153|186->154|186->154|186->154|187->155|187->155|187->155|189->157|189->157|189->157|194->162|195->163|196->164|197->165|202->170|202->170|202->170
+                  HASH: fd62a59f5431b6b3f93414059b031c28cbc632e9
+                  MATRIX: 981->1|1133->58|1163->62|1306->178|1321->184|1387->229|4880->3695|4914->3712|4955->3714|4989->3721|5015->3737|5056->3739|5091->3746|5124->3751|5135->3752|5165->3760|5221->3788|5232->3789|5269->3804|5317->3824|5328->3825|5359->3834|5407->3854|5418->3855|5449->3864|5513->3900|5529->3906|5589->3944|5743->4067|5778->4074|5821->4086|5854->4091|6155->4365|6189->4382|6230->4384|6264->4391|6290->4407|6331->4409|6366->4416|6399->4421|6410->4422|6440->4430|6496->4458|6507->4459|6544->4474|6592->4494|6603->4495|6634->4504|6682->4524|6693->4525|6724->4534|6788->4570|6804->4576|6864->4614|7018->4737|7053->4744|7096->4756|7129->4761|7430->5035|7464->5052|7505->5054|7539->5061|7565->5077|7606->5079|7641->5086|7674->5091|7685->5092|7715->5100|7771->5128|7782->5129|7819->5144|7867->5164|7878->5165|7909->5174|7957->5194|7968->5195|7999->5204|8063->5240|8079->5246|8139->5284|8293->5407|8328->5414|8371->5426|8404->5431|8492->5491|8508->5497|8558->5525
+                  LINES: 28->1|33->1|37->5|43->11|43->11|43->11|136->104|136->104|136->104|137->105|137->105|137->105|138->106|138->106|138->106|138->106|140->108|140->108|140->108|141->109|141->109|141->109|142->110|142->110|142->110|144->112|144->112|144->112|149->117|150->118|151->119|152->120|158->126|158->126|158->126|159->127|159->127|159->127|160->128|160->128|160->128|160->128|162->130|162->130|162->130|163->131|163->131|163->131|164->132|164->132|164->132|166->134|166->134|166->134|171->139|172->140|173->141|174->142|180->148|180->148|180->148|181->149|181->149|181->149|182->150|182->150|182->150|182->150|184->152|184->152|184->152|185->153|185->153|185->153|186->154|186->154|186->154|188->156|188->156|188->156|193->161|194->162|195->163|196->164|201->169|201->169|201->169
                   -- GENERATED --
               */
           
